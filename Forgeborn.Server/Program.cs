b2699e2a11f865c6d97c8761ed45cbc3c2690a81
Robert Gabriel Macalintal <maca0144@algonquinlive.com>
2025-09-24 11:55:27 -0400

    Updated dependencies as needed

--- Forgeborn.Server/Program.cs
+++ Forgeborn.Server/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
