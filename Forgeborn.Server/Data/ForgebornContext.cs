2c8f6290ab2d9f40d496fecb22a0071b853eeb25
Robert Gabriel Macalintal <maca0144@algonquinlive.com>
2025-09-24 11:54:16 -0400

    Added Controller, Service files, and DbContext for User class

---
+++ Forgeborn.Server/Data/ForgebornContext.cs
@@ -0,0 +1,15 @@
+﻿using Forgeborn.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApp.Data
+{
+    public class ForgebornContext : DbContext
+    {
+        public ForgebornContext(DbContextOptions<ForgebornContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Forgeborn.Server/Data/Service/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Forgeborn.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApp.Data.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly ForgebornContext _context;
+
+        public UserService(ForgebornContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+    }
+}
