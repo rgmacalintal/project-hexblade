789fbf77c4a22eaa25b0eb0903e2c1a35cffab03
Robert Gabriel Macalintal <maca0144@algonquinlive.com>
2025-10-09 01:00:59 -0400

    Added Armor subclass

---
+++ Forgeborn.Server/Models/Items/Armor.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Forgeborn.Server.Models.Items
+{
+    public class Armor : Item
+    {
+        private static string defaultTraits = "{" +
+            "'shield' : false," +
+            "'light' : true" +
+            "'medium' : false" +
+            "'heavy' : false" +
+        "}";
+
+        public string Effect { get; set; } = null!;
+        public int ArmorClass { get; set; } = 0;
+        public JObject Traits { get; set; } = JObject.Parse(defaultTraits);
+
+    }
+}
