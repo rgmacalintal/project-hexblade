4675b96c811dd10ce6d963b3b24b3efe188093ea
Robert Gabriel Macalintal <maca0144@algonquinlive.com>
2025-10-15 09:47:18 -0400

    Created default Controllers and made some fixes

--- Forgeborn.Server/Models/Characters.cs
+++ Forgeborn.Server/Models/Characters.cs
@@ -7,10 +7,8 @@ namespace Forgeborn.Server.Models
 {
     public class Characters
     {
-        private static string emptyInventory = "{" +
-            "'contents' : { }" +
-        "}";
-        [Key]
+        private static string defaultInventory = "{ " +
+            "'inventory' : { } }";
 
         public int Id { get; set; }
         [Required]
@@ -20,8 +18,8 @@ namespace Forgeborn.Server.Models
         [Required]
         public string Race { get; set; } = null!;
         [Required]
-        public JObject Stats { get; set; } = null!;
-        public JObject Inventory { get; set; } = JObject.Parse(emptyInventory);
+        public JsonContent Stats { get; set; } = null!;
+        public JsonContent[] Inventory { get; set; } = [];
         public string Background { get; set; } = null!;
         public string Journal { get; set; } = null!;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
