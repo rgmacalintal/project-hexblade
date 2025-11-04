f9ca735a0ddb87d384513d5a2a10f37404e63e0d
Robert Gabriel Macalintal <maca0144@algonquinlive.com>
2025-11-04 14:21:07 -0500

    Some Model changes

--- Forgeborn.Server/Models/Characters.cs
+++ Forgeborn.Server/Models/Characters.cs
@@ -44,9 +44,7 @@ namespace Forgeborn.Server.Models
         public int UserId { get; set; }
         [ForeignKey(nameof(UserId))]
         public Users? User { get; set; }
-        public int RulesetId { get; set; }
-        [ForeignKey(nameof(RulesetId))]
-        public Rulesets? Ruleset { get; set; }
+        public ICollection<Rulesets> Players { get; set; } = new List<Rulesets>();
 
     }
 }
