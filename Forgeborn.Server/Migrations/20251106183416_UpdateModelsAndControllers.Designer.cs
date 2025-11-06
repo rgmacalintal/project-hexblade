4979559dd4e2ac1b8bbfdcf456c32df9e861e9f2
Aaron Solomon <solo0069@algonquinlive.com>
2025-11-06 13:51:55 -0500

    UpdateModelsAndControllers Migration

--- Forgeborn.Server/Migrations/20251106183416_UpdateModelsAndControllers.Designer.cs
+++ Forgeborn.Server/Migrations/20251106183416_UpdateModelsAndControllers.Designer.cs
@@ -5,6 +5,7 @@ using Forgeborn.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,9 +13,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Forgeborn.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251106183416_UpdateModelsAndControllers")]
+    partial class UpdateModelsAndControllers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Forgeborn.Server/Migrations/20251106183416_UpdateModelsAndControllers.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forgeborn.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelsAndControllers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Rulesets_RulesetId",
+                table: "Characters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lobbys_Players_PlayerId",
+                table: "Lobbys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lobbys_PlayerId",
+                table: "Lobbys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_RulesetId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Lobbys");
+
+            migrationBuilder.DropColumn(
+                name: "RulesetId",
+                table: "Characters");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Attack",
+                table: "Weapons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attack",
+                table: "Weapons");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId",
+                table: "Lobbys",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RulesetId",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lobbys_PlayerId",
+                table: "Lobbys",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_RulesetId",
+                table: "Characters",
+                column: "RulesetId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Rulesets_RulesetId",
+                table: "Characters",
+                column: "RulesetId",
+                principalTable: "Rulesets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lobbys_Players_PlayerId",
+                table: "Lobbys",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
