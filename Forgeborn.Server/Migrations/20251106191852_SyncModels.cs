789e90e88fa7f242e7d860b81363e5326f2525b8
Aaron Solomon <solo0069@algonquinlive.com>
2025-11-06 14:20:40 -0500

    Pushed SyncModels Migration
    might considering freshly deleting all migrations, dropping the db, and making a new initial migration again since a lot has been changed

---
+++ Forgeborn.Server/Migrations/20251106191852_SyncModels.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forgeborn.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
